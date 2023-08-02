b71e7466ba9ebbe2d899e474532e313709a5e2bd
Helyaby <vieiracida722@gmail.com>
2023-08-02 19:43:13 -0300

    criado para calcular a média

---
+++ media.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Digite sua primeira nota :\n"))
+nota2 = float(input("Digite sua segunda nota :\n"))
+media = (nota1 + nota2) /2
+print(f"Sua média é de {media} pontos")
+
+
+#para upar o codigo para o github colocar: git add
+#git commit -m " "
+#git dush
